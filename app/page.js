0948c3a514f6eb2154d258b89892a823a0c0cd87
danirogerc <danirogerc@gmail.com>
2024-07-03 13:23:25 +0200

    new update

--- app/page.js
+++ app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Get Started</button>
+      <h1>Collect user feedback to make a better product</h1>
+      <div>
+        Create a feedback board in minutes to be able to prioritize what
+        features users love
+      </div>
     </main>
   );
 }
